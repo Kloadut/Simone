a7716cb0782860bcf581b480a632520be171cfdc
Kload <kload@kload.fr>
2013-11-16 00:43:35 +0100

    bugfix

--- save.php
+++ save.php
@@ -5,6 +5,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_SERVER['PHP_AUTH_USER'])) {
     @ini_set('implicit_flush',1);
     @ob_end_clean();
     set_time_limit(120);
+    ob_implicit_flush(1);
     $user = $_SERVER['PHP_AUTH_USER'];
     $path = dirname(__FILE__);
     $action = 'Add';
