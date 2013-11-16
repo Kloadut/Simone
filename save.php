7ec2f247ee4515a259a7994d9502d065ba47d29d
Kload <kload@kload.fr>
2013-11-16 11:52:42 +0100

    Security checks

--- save.php
+++ save.php
@@ -9,30 +9,35 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_SERVER['PHP_AUTH_USER'])) {
     echo str_pad('', 4096); flush();
     sleep(1);
     $user = $_SERVER['PHP_AUTH_USER'];
+    $page = $_POST["page"];
+    if (!preg_match('/^[A-Za-z0-9_]+$/', $page)) {
+        die;
+    }
+    $content = $_POST["content"]
     $path = dirname(__FILE__);
     $action = 'Add';
-    if (file_exists($path.'/_pages/'. $_POST["page"] .'.md')) {
-        if ($_POST["content"] == '') {
-            echo 'Deleting '. $_POST["page"] .'.md ... '.str_pad('', 4096); flush();
-            unlink($path.'/_pages/'. $_POST["page"] .'.md');
+    if (file_exists($path.'/_pages/'. $page .'.md')) {
+        if ($content == '') {
+            echo 'Deleting '. $page .'.md ... '.str_pad('', 4096); flush();
+            unlink($path.'/_pages/'. $page .'.md');
             $action = 'Remove';
             echo "OK<br />".str_pad('', 4096); flush();
         } else {
             $action = 'Update';
         }
     }
-    if ($action != 'Remove' && $_POST["content"] != '') {
-        echo 'Writing '. $_POST["page"] .'.md ... '.str_pad('', 4096); flush();
-        $file = fopen($path.'/_pages/'. $_POST["page"] .'.md', 'w');
-        fwrite($file, $_POST["content"]);
+    if ($action != 'Remove' && $content != '') {
+        echo 'Writing '. $page .'.md ... '.str_pad('', 4096); flush();
+        $file = fopen($path.'/_pages/'. $page .'.md', 'w');
+        fwrite($file, $content);
         fclose($file);
         echo "OK<br />".str_pad('', 4096); flush();
     }
     echo 'Pulling changes ... '.str_pad('', 4096); flush();
-    exec('cd '.$path.'/_pages && git pull', $result, $result_code);
+    exec(escapeshellcmd('cd '.$path.'/_pages && git pull'), $result, $result_code);
     if (!$result_code) { echo "OK<br />".str_pad('', 4096); flush(); }
     echo 'Committing changes ... '.str_pad('', 4096); flush();
-    exec('cd '.$path.'/_pages && git add --all . && git commit -m "'.$action.' '.$_POST["page"] .'.md" --author="'.$user.' <'.$user.'>"', $result, $result_code);
+    exec(escapeshellcmd('cd '.$path.'/_pages && git add --all . && git commit -m "'.$action.' '.$page .'.md" --author="'.$user.' <'.$user.'>"'), $result, $result_code);
     if (!$result_code) { echo "OK<br />".str_pad('', 4096); flush(); }
 
     // Only if you have a SSH key without password
@@ -40,7 +45,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_SERVER['PHP_AUTH_USER'])) {
     exec('cd '.$path.'/_pages && git push', $result, $result_code); // Only if you have a SSH key without password
     if (!$result_code) { echo "OK<br />".str_pad('', 4096); flush(); }
     echo 'Generating RSS ... '.str_pad('', 4096); flush();
-    exec('ruby '.$path.'/gitrss.rb '.$path.'/_pages http://doc.yunohost.org/ "YunoHost documentation" > '.$path.'/feed.rss', $result, $result_code);
+    exec(escapeshellcmd('ruby '.$path.'/gitrss.rb '.$path.'/_pages http://doc.yunohost.org/ "YunoHost documentation" > '.$path.'/feed.rss'), $result, $result_code);
     if (!$result_code) { echo "OK<br />".str_pad('', 4096); flush(); }
 } else {
     header($_SERVER['SERVER_PROTOCOL'].' 401 UNAUTHORIZED');
